eb8b610dd8943c94fc1325dd0af1535ff520f80e
mfaizk <suneater1413@gmail.com>
2022-11-26 20:49:14 +0530

    overhaul completed now todo will be logged user specific

--- api/controller/todoController/getTaskController.js
+++ api/controller/todoController/getTaskController.js
@@ -2,26 +2,26 @@ const Todo = require("../../model/todoModel");
 
 const getTaskController = async (req, res) => {
   const { id, sortBy } = req.params;
+  const data = await Todo.findById(req.user.id);
+  const todoIndex = data.todos.findIndex((e) => e._id == id);
+  const { task } = data.todos[todoIndex];
   if (sortBy == "default") {
     try {
-      const todo = await Todo.findById(id);
-      return res.status(200).json(todo.task);
+      return res.status(200).json(task);
     } catch (error) {
       return res.send(error.message);
     }
   } else if (sortBy == "ctime") {
     try {
-      const todo = await Todo.findById(id);
-      const task = todo.task.sort((x, y) => y.createdAt - x.createdAt);
-      return res.send(task);
+      const sortedTask = task.sort((x, y) => x.createdAt - y.createdAt);
+      return res.send(sortedTask);
     } catch (error) {
       return res.send(error.message);
     }
   } else if (sortBy == "uptime") {
     try {
-      const todo = await Todo.findById(id);
-      const task = todo.task.sort((x, y) => y.updatedAt - x.updatedAt);
-      return res.send(task);
+      const sortedTask = task.sort((x, y) => x.updatedAt - y.updatedAt);
+      return res.send(sortedTask);
     } catch (error) {
       return res.send(error.message);
     }
