e3a9614e4bf73a1b045a5f9b72129da3cf593f48
mfaizk <suneater1413@gmail.com>
2022-11-26 20:54:04 +0530

    middleware condition updated

--- api/controller/authController/authMiddleWare.js
+++ api/controller/authController/authMiddleWare.js
@@ -2,6 +2,10 @@ const jwt = require("jsonwebtoken");
 const User = require("../../model/userModel");
 const auth = async (req, res, next) => {
   const { SECRET } = process.env;
+  if (Object.keys(req.cookies).length < 1) {
+    return res.status(401).send({ msg: "Login require" });
+  }
+  console.log(Object.keys(req.cookies));
   const token = Object.keys(req.cookies);
   const data = jwt.decode(token[0], SECRET);
   if (!data) return res.status(401).send({ msg: "invalid cookie" });
