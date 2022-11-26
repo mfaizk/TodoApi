5c3fa417427d9e0e9749ab5027e70d2a2e95d737
mfaizk <suneater1413@gmail.com>
2022-11-26 20:39:49 +0530

    delete task updated

--- api/controller/todoController/deleteTaskController.js
+++ api/controller/todoController/deleteTaskController.js
@@ -2,13 +2,18 @@ const Todo = require("../../model/todoModel");
 
 const deleteTaskController = async (req, res) => {
   const { todoId, taskId } = req.params;
-
   try {
-    let todo = await Todo.findById(todoId);
-    let taskIndex = todo.task.findIndex((e) => e._id == taskId);
-    todo.task.splice(taskIndex, 1);
-    await todo.save();
-    res.status(200).send(todo);
+    const data = await Todo.findById(req.user.id);
+    const todoIndex = data.todos.findIndex((e) => e._id == todoId);
+    const taskIndex = data.todos[todoIndex].task.findIndex(
+      (e) => e._id == taskId
+    );
+
+    data.todos[todoIndex].task.splice(taskIndex, 1);
+    await data.save();
+    res.status(201).send({
+      msg: "Success",
+    });
   } catch (error) {
     res.send(error.message);
   }
--- api/controller/todoController/editTaskController.js
+++ api/controller/todoController/editTaskController.js
@@ -3,16 +3,6 @@ const Todo = require("../../model/todoModel");
 const editTaskController = async (req, res) => {
   const { todoId, taskId } = req.params;
   const { editedtask } = req.body;
-
-  // try {
-  //   const todo = await Todo.findById(todoId);
-  //   const taskIndex = todo.task.findIndex((e) => e._id == taskId);
-  //   todo.task[taskIndex].task = editedtask;
-  //   todo.save();
-  //   res.send(todo);
-  // } catch (error) {
-  //   res.send(error.message);
-  // }
   try {
     const data = await Todo.findById(req.user.id);
     if (!data) return res.send({ msg: "todo not found" });
